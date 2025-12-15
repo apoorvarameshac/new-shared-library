c48e37e84ba6458dbe09ea29d262c979595e598b
apoorvarameshac <76146612+apoorvarameshac@users.noreply.github.com>
2025-12-15 14:05:28 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Deploying container from image..."
-    sh "docker run -d --name simpleapp_container simpleapp:latest"
+    sh "docker run -d --name simpleapp_container simple-app-img"
     echo "Container deployed: simpleapp_container"
 }
 
