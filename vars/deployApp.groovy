de321c2f497dbc84abd7b19c7dd8bd71016b31b8
apoorvarameshac <76146612+apoorvarameshac@users.noreply.github.com>
2025-12-15 14:06:52 +0530

    Expose port 8081 in the container deployment

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Deploying container from image..."
-    sh "docker run -d --name simpleapp_container simple-app-img"
+    sh "docker run -d --name simpleapp_container -p 8082:8081 simple-app-img"
     echo "Container deployed: simpleapp_container"
 }
 
